ebd18b5a0c290683b0b1426f90150971784f199d
Ilya <sia.ilya.2017@gmail.com>
2021-02-06 19:54:26 +0300

    - Сделана механика с карточками - Сделан онбордин для карточек

---
+++ src/components/QuestionCard.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import TinderCard from "react-tinder-card";
+import {Button} from "@vkontakte/vkui";
+
+
+const QuestionCard = React.forwardRef(({currentIndex, questionSwiped, questionIndex, questionItem}, ref) => {
+    const adsOrQuestion = (questionItem.type === "ads" ? "ads-card" : "question-card")
+    const activeClass = (currentIndex === questionIndex) ? (adsOrQuestion + " active-question") : (adsOrQuestion + " question-card")
+
+    return (
+        <TinderCard
+            ref={ref}
+            preventSwipe={["up", "down"]}
+            onSwipe={(e) => {
+                questionSwiped(questionIndex, e)
+            }}
+            className={activeClass}
+        >
+            <div className="text-center question-index">Вопрос {questionIndex} из 20</div>
+            <div className="text-center question-text">
+                <div className="question-text-box">
+                    {questionItem.text}
+                </div>
+                {
+                    questionItem.type === "ads" &&
+                    <Button href={questionItem.link} target="_blank" style={{marginTop: 20, border: "solid 2px white"}}>
+                        {questionItem.action}
+                    </Button>
+                }
+            </div>
+        </TinderCard>
+    )
+})
+
+export default QuestionCard;
