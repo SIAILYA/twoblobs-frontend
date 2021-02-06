d36500583fd50986744b1b9f68644e930042a243
Ilya <sia.ilya.2017@gmail.com>
2021-02-06 23:41:15 +0300

    - Экран с тестом и сам тест (+ вопросы) - Баннер, ведущий в тест

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import '@vkontakte/vkui/dist/vkui.css';
 import bridge from "@vkontakte/vk-bridge";
 
 import {
-	Div, Header, Group, PanelHeader, View, AppRoot,
-	Button, Cell, Epic, Panel, Root, Tabbar, TabbarItem,
+	Div, Header, PanelHeader, View, AppRoot,
+	Button, Epic, Panel, Root, Tabbar, TabbarItem,
 	Avatar, Card, SimpleCell, RichCell, PanelHeaderBack,
-	Radio, FormLayout, FormItem, Gallery
+	Radio, FormLayout, Gallery, FormStatus
 } from "@vkontakte/vkui";
 
 import {
@@ -28,6 +28,7 @@ import BannerBlock from "./components/BannerBlock";
 import QuestionCard from "./components/QuestionCard";
 
 import banner_reg_big from "./img/banner_reg_big.png"
+import banner_knw_big from "./img/banner_knw_big.png"
 import hand from "../src/img/privacy_outline_28.svg"
 import phone from "../src/img/smartphone_outline_28.svg"
 import ok from "../src/img/thumbs_up_outline_28.svg"
@@ -77,8 +78,47 @@ const questions = [
 	{type: "question", text: "Я люблю хлеб"}
 ]
 
+const knowledge = [
+	{
+		question: "Кто может стать донором костного мозга?",
+		variants: ["Любой гражданин РФ в возрасте от 14 до 45 лет", "Любой совершеннолетний здоровый гражданин РФ", "Любой здоровый гражданин РФ без хронических заболеваний в возрасте от 18 до 45 лет весом более 50кг", "Любой гражданин РФ без хронических заболеваний"],
+		correct: "Любой здоровый гражданин РФ без хронических заболеваний в возрасте от 18 до 45 лет весом более 50кг",
+		description: "Донором может стать каждый здоровый гражданин РФ, достигший совершеннолетия и имеющий массу тела более 50 килограмм"
+	},
+	{
+		question: "По статистике, генетическим двойником может стать только 1 человек из ...",
+		variants: ["10000", "50000", "100000", "100"],
+		correct: "10000",
+		description: "Шанс найти донора с подходящим костным мозгом - 0,01%. А встретить двойника, у которого полностью совпадет HLA генотип - 1 на 500000!"
+	},
+	{
+		question: "Трудно ли восстановиться после донорства?",
+		variants: ["Трудно", "Легко"],
+		correct: "Легко",
+		description: "Потеря части стволовых клеток донором не ощущается, и их объем полностью восстанавливается в течение 7—10 дней!"
+	},
+	{
+		question: "Правда ли, что донором костного мозга можно стать только один раз?",
+		variants: ["Да", "Нет"],
+		correct: "Нет",
+		description: "Способность к регенерации стволовых клеток настолько высока, что при необходимости донором можно становиться несколько раз в жизни без последствий для здоровья!"
+	},
+	{
+		question: "Костный мозг - это ...",
+		variants: ["То же самое, что и спинной мозг", "Полужидкое вещество внутри костей человека, играющее ключевую роль в выработке клеток крови"],
+		correct: "Полужидкое вещество внутри костей человека, играющее ключевую роль в выработке клеток крови",
+		description: "Костный и спинной мозг - совершенно разные органы! Костный мозг - важнейший орган кроветворной системы, осуществляющий гемопоэз, или кроветворение - процесс создания новых клеток крови!"
+	},
+	{
+		question: "Правда ли, что есть только 2 способа забора костного мозга?",
+		variants: ["Да", "Нет, костный мозг забирается только одним способом", "Нет, костный мозг можно забрать множеством способов"],
+		correct: "Да",
+		description: "Костный мозг забирается только двумя способами: с хирургическим вмешательством забирается ткань из тазобедренной кости, либо забирается некоторое количество крови пациента"
+	}
+]
+
 const App = () => {
-	const [activeView, setActiveView] = useState("knowledge-test");
+	const [activeView, setActiveView] = useState("main");
 	const [activePanel, setActivePanel] = useState("main");
 	const [activeArticlePanel, setActiveArticlePanel] = useState("feed");
 	const [activeStory, setActiveStory] = useState("match");
@@ -88,7 +128,11 @@ const App = () => {
 	const [questionIndex, setQuestionIndex] = useState(1)
 	const [answers, setAnswers] = useState([])
 	const [slideIndex, setSlideIndex] = useState(0)
+	const [knowledgeAnswers, setKnowledgeAnswers] = useState(Array(knowledge.length).fill(null))
+	const [knowledgeResult, setKnowledgeResult] = useState(0)
+
 	const childRefs = useMemo(() => Array(questions.length).fill(0).map(i => React.createRef()), [])
+
 
 	useEffect(() => {
 		bridge.send("VKWebAppInit")
@@ -130,6 +174,19 @@ const App = () => {
 
 	function answerQuestion(answer) {
 		childRefs[questions.length - questionIndex].current.swipe(answer === 1 ? "right" : "left")
+	}
+
+	function answerKnowledge(index, item, answer) {
+		console.log(item.correct === answer)
+		let new_answers = JSON.parse(JSON.stringify(knowledgeAnswers))
+		new_answers[index] = (item.correct === answer)
+		setKnowledgeAnswers(new_answers)
+		console.log(knowledgeAnswers)
+	}
+
+	function goResults() {
+		setActivePanel("results")
+		setKnowledgeResult(knowledgeAnswers.filter(value => value === true).length)
 	}
 
 	return (
@@ -223,9 +280,9 @@ const App = () => {
 							</View>
 							<View id="match" activePanel="match">
 								<Panel id="match">
-									<PanelHeader>Искать пару</PanelHeader>
+									<PanelHeader>Тесты</PanelHeader>
 									<Div className="d-flex">
-										<div style={{width: 20, backgroundColor: "#aeaeae", marginRight: 10}}>
+										<div style={{width: "20px", backgroundColor: "#aeaeae", marginRight: 10}}>
 										</div>
 										<div style={{fontStyle: "italic", fontSize: "13px"}}>
 											Костный мозг у каждого свой, и найти подходящего донора с подходящими стволовыми клетками не так просто, как найти человека с такой же группой крови.
@@ -241,6 +298,25 @@ const App = () => {
 										image={banner_reg_big}
 										button="Пройти тест"
 										action={() => {setActiveView("cards-test"); setActivePanel("cards-onboarding")}}
+									>
+									</BannerBlock>
+
+									<Div className="d-flex">
+										<div style={{width: "7px", backgroundColor: "#aeaeae", marginRight: 10}}>
+										</div>
+										<div style={{fontStyle: "italic", fontSize: "13px"}}>
+											Прочитал все статьи из базы знаний?
+											Проверь свои знания в области донорства костного мозга!
+										</div>
+									</Div>
+									<BannerBlock
+										size="m"
+										class="banner_main"
+										header="Проверка знаний"
+										subheader="Узнай, как хорошо ты знаешь устройство донорской системы!"
+										image={banner_knw_big}
+										button="Пройти тест"
+										action={() => {setActiveView("knowledge-test"); setActivePanel("test")}}
 									>
 									</BannerBlock>
 								</Panel>
@@ -283,96 +359,124 @@ const App = () => {
 						</Epic>
 					</Panel>
 				</View>
+
 				<View id="onboarding" activePanel="onboarding-panel" popout={popout}>
-					<Panel id="onboarding-panel">
-						<PanelHeader>Две капли 2 панель</PanelHeader>
-						<Group header={<Header mode="secondary">Три капли</Header>}>
-							<Cell>Привет</Cell>
-							<Cell>Одна капля</Cell>
-							<Button onClick={() => {setActivePanel("main")}}>Переход на другую панель</Button>
-						</Group>
-					</Panel>
+
 				</View>
 
-				<View id="knowledge-test" activePanel="test">
+				<View id="knowledge-test" activePanel={activePanel}>
 					<Panel id="test">
 						<PanelHeader>Пробный тест</PanelHeader>
 						<Gallery
+							slideWidth="100%"
+							align="center"
+							className="gallery-knowledge"
+							slideIndex={slideIndex}
+							isDraggable
+							onChange={slideIndexChange => setSlideIndex(slideIndexChange)}
 						>
-							<Div>
-								<FormLayout>
-									По статистике, по HLA-фенотипу подходит только 1 человек из ... ?
-									<FormItem>
-										<Radio name="test" value="1">500000</Radio>
-										<Radio name="test" value="2">50000</Radio>
-										<Radio name="test" value="3">1000000</Radio>
-										<Radio name="test" value="4">300000</Radio>
-									</FormItem>
-								</FormLayout>
-							</Div>
-							<Div>
-								<FormLayout>
-									Кто может стать донором?
-									<FormItem>
-										<Radio name="test" value="1">Любой гражданин РФ в возрасте от 14 до 45 лет</Radio>
-										<Radio name="test" value="2">Любой гражданин РФ</Radio>
-										<Radio name="test" value="3">Любой здоровый гражданин РФ без хронических заболеваний в возрасте от 18 до 45 лет</Radio>
-										<Radio name="test" value="4">Любой гражданин РФ без хронических заболеваний</Radio>
-									</FormItem>
-								</FormLayout>
-							</Div>
-							<Div>
-								<FormLayout>
-									Трудно ли восстановиться после донорства?
-									<FormItem>
-										<Radio name="test" value="1">Трудно</Radio>
-										<Radio name="test" value="2">Легко</Radio>
-									</FormItem>
-								</FormLayout>
-							</Div>
-							<Div>
-								<FormLayout>
-									Правда ли, что донором костного мозга можно стать только один раз?
-									<FormItem>
-										<Radio name="test" value="1">Да</Radio>
-										<Radio name="test" value="2">Нет</Radio>
-									</FormItem>
-								</FormLayout>
-							</Div>
-							<Div>
-								<FormLayout>
-									*Заглужка* Верна ли статистика:
-									<FormItem>
-										<Radio name="test" value="1">??</Radio>
-										<Radio name="test" value="2">??</Radio>
-										<Radio name="test" value="3">??</Radio>
-										<Radio name="test" value="4">??</Radio>
-									</FormItem>
-								</FormLayout>
-							</Div>
-							<Div>
-								<FormLayout>
-									*Заглужка* ???:
-									<FormItem>
-										<Radio name="test" value="1">??</Radio>
-										<Radio name="test" value="2">??</Radio>
-										<Radio name="test" value="3">??</Radio>
-										<Radio name="test" value="4">??</Radio>
-									</FormItem>
-								</FormLayout>
-							</Div>
-							<Div>
-								<FormLayout>
-									*Заглужка* ???:
-									<FormItem>
-										<Radio name="test" value="1">??</Radio>
-										<Radio name="test" value="2">??</Radio>
-										<Radio name="test" value="3">??</Radio>
-										<Radio name="test" value="4">??</Radio>
-									</FormItem>
-								</FormLayout>
-							</Div>
+							{
+								knowledge.map((item, index) => {
+									return(
+										<div key={index}>
+											<FormLayout style={{padding: 10, display: "flex", flexDirection: "column"}}>
+												<div className="text-center knowledge-question">{item.question}</div>
+												<div className="knowledge-answers">
+													{
+														knowledgeAnswers[index] === null ?
+														item.variants.map((answer, answer_index) => {
+															return(
+																<Radio key={answer_index}
+																	   name="test"
+																	   onClick={(e) => {
+																	   	if (knowledgeAnswers[index] === null) answerKnowledge(index, item, e.currentTarget.value)
+																	   }}
+																	   value={answer}
+																>
+																	{answer}
+																</Radio>
+															)
+														}) :
+														item.variants.map((answer, answer_index) => {
+															return(
+																<Radio key={answer_index}
+																	   name="test"
+																	   checked={item.correct === answer}
+																	   className={item.correct === answer ? "correct-variant" : "uncorrect-variant"}
+																>
+																	{answer}
+																</Radio>
+															)
+														})
+													}
+													{
+														knowledgeAnswers[index] === true &&
+														<FormStatus className="form-status-answer" mode="correct" header="Правильно!">
+															{item.description}
+														</FormStatus>
+													}
+													{
+														knowledgeAnswers[index] === false &&
+														<FormStatus className="form-status-answer" mode="error" header="А вот и нет!">
+															{item.description}
+														</FormStatus>
+													}
+												</div>
+											</FormLayout>
+										</div>
+									)
+								})
+							}
 						</Gallery>
+						<Div>
+							<Button stretched
+									size="l"
+									disabled={(slideIndex === (knowledge.length - 1)) && (knowledgeAnswers.some((value => {return (value == null)})))}
+									onClick={() => {
+										slideIndex < (knowledge.length - 1) ?
+										setSlideIndex((prev) => {return (prev + 1)}) :
+										goResults();
+									}}>
+								{
+									slideIndex === (knowledge.length - 1) ? "Закончить" :"Дальше"
+								}
+							</Button>
+						</Div>
+					</Panel>
+					<Panel id="results">
+						<PanelHeader left={<PanelHeaderBack onClick={() => {setActiveView("main")}}/>}>Результаты</PanelHeader>
+						<div className="results-knowledge-header text-center">Вы ответили правильно на</div>
+						<div className="results-knowledge">
+							{knowledgeResult}/{knowledge.length}
+						</div>
+						<div className="text-center results-knowledge-questions">
+							вопросов
+						</div>
+						<Div className="results-knowledge-caption text-center">
+							{
+								knowledgeResult <= 2 &&
+									"Стоит подучить матчасть!"
+							}
+							{
+								(knowledgeResult > 2) && (knowledgeResult <= 4) &&
+									"Неплохо, но не идеал!"
+							}
+							{
+								(knowledgeResult > 4) && (knowledgeResult <= 5) &&
+									"Отлично!"
+							}
+							{
+								(knowledgeResult > 5) && (knowledgeResult <= 6) &&
+									"Отлично!"
+							}
+						</Div>
+						<Div style={{marginTop: "25vh"}}>
+							<Button stretched size="l" onClick={() => {setActiveView("main")}}>
+								Домой
+							</Button>
+						</Div>
+					</Panel>
+				</View>
 				<View id="cards-test" activePanel={activePanel}>
 					<Panel id="cards-onboarding">
 						<Gallery
